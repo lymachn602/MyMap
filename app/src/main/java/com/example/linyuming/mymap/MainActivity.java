8e880ea20b02b7340dee1dc8cf7cf8e669fc2159
lymachn602 <287643905@qq.com>
2016-12-17 19:45:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linyuming/mymap/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.linyuming.mymap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+    }
+}
